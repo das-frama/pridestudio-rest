ca6a728ef446a5f63e9bcceac7dc47cab508c413
Andrey Galaktionov <das.frama@gmail.com>
2019-06-19 02:03:12 +0300

    Обновление БД.

--- data/db.sql
+++ data/db.sql
@@ -1,140 +1,161 @@
--- phpMyAdmin SQL Dump
--- version 4.8.3
--- https://www.phpmyadmin.net/
---
--- Хост: 127.0.0.1:3306
--- Время создания: Июн 16 2019 г., 02:42
--- Версия сервера: 10.3.9-MariaDB
--- Версия PHP: 7.2.10
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- База данных: `pridestudio_true`
---
-
+-- --------------------------------------------------------
+-- Хост:                         127.0.0.1
+-- Версия сервера:               8.0.16 - MySQL Community Server - GPL
+-- Операционная система:         Win64
+-- HeidiSQL Версия:              10.2.0.5599
 -- --------------------------------------------------------
 
---
--- Структура таблицы `client`
---
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
+-- Дамп структуры для таблица pridetstudio-true.client
 CREATE TABLE IF NOT EXISTS `client` (
-  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `sex` tinyint(4) DEFAULT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `phone` varchar(32) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `comment` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `phone` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `comment` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Клиент';
 
--- --------------------------------------------------------
+-- Экспортируемые данные не выделены.
 
---
--- Структура таблицы `hall`
---
-
+-- Дамп структуры для таблица pridetstudio-true.hall
 CREATE TABLE IF NOT EXISTS `hall` (
-  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `slug` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `description` varchar(1024) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `slug` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `description` varchar(1024) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `base_price` decimal(10,2) NOT NULL,
-  `preview_image` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `detail_image` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `sort` int(11) NOT NULL DEFAULT 1,
-  `is_active` tinyint(1) NOT NULL DEFAULT 1,
-  `created_at` int(10) UNSIGNED NOT NULL,
-  `updated_at` int(10) UNSIGNED NOT NULL,
-  `created_by` int(10) UNSIGNED DEFAULT NULL,
-  `updated_by` int(10) UNSIGNED DEFAULT NULL,
+  `preview_image` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `detail_image` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `sort` int(11) NOT NULL DEFAULT '1',
+  `is_active` tinyint(1) NOT NULL DEFAULT '1',
+  `created_at` int(10) unsigned NOT NULL,
+  `updated_at` int(10) unsigned NOT NULL,
+  `created_by` int(10) unsigned DEFAULT NULL,
+  `updated_by` int(10) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `created_by` (`created_by`),
-  KEY `updated_by` (`updated_by`)
+  KEY `updated_by` (`updated_by`),
+  CONSTRAINT `FK_hall_created_by` FOREIGN KEY (`created_by`) REFERENCES `user` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `FK_hall_updated_by` FOREIGN KEY (`updated_by`) REFERENCES `user` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Зал';
 
--- --------------------------------------------------------
+-- Экспортируемые данные не выделены.
 
---
--- Структура таблицы `payment`
---
-
+-- Дамп структуры для таблица pridetstudio-true.payment
 CREATE TABLE IF NOT EXISTS `payment` (
-  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `record_id` int(10) UNSIGNED DEFAULT NULL,
-  `client_id` int(10) UNSIGNED DEFAULT NULL,
-  `method_id` int(10) UNSIGNED DEFAULT NULL,
-  `type_id` int(10) UNSIGNED DEFAULT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `record_id` int(10) unsigned DEFAULT NULL,
+  `client_id` int(10) unsigned DEFAULT NULL,
+  `method_id` int(10) unsigned DEFAULT NULL,
+  `type_id` int(10) unsigned DEFAULT NULL,
   `amount` decimal(10,2) NOT NULL,
-  `status` tinyint(4) NOT NULL DEFAULT 1,
-  `paid_at` int(10) UNSIGNED DEFAULT NULL,
-  `created_at` int(10) UNSIGNED NOT NULL,
-  `updated_at` int(10) UNSIGNED NOT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT '1',
+  `paid_at` int(10) unsigned DEFAULT NULL,
+  `created_at` int(10) unsigned NOT NULL,
+  `updated_at` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`),
   KEY `client_id` (`client_id`),
   KEY `type_id` (`type_id`),
   KEY `method_id` (`method_id`),
-  KEY `record_id` (`record_id`) USING BTREE
+  KEY `record_id` (`record_id`) USING BTREE,
+  CONSTRAINT `payment_ibfk_1` FOREIGN KEY (`client_id`) REFERENCES `client` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `payment_ibfk_2` FOREIGN KEY (`record_id`) REFERENCES `record` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Оплата';
 
--- --------------------------------------------------------
+-- Экспортируемые данные не выделены.
 
---
--- Структура таблицы `record`
---
+-- Дамп структуры для таблица pridetstudio-true.promo
+CREATE TABLE IF NOT EXISTS `promo` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `code` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `discount` float NOT NULL,
+  `starts_at` int(11) unsigned DEFAULT NULL,
+  `ends_at` int(11) unsigned DEFAULT NULL,
+  `date` datetime DEFAULT NULL,
+  `length` int(11) DEFAULT NULL,
+  `is_active` tinyint(1) DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+-- Экспортируемые данные не выделены.
+
+-- Дамп структуры для таблица pridetstudio-true.record
 CREATE TABLE IF NOT EXISTS `record` (
-  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `client_id` int(10) UNSIGNED DEFAULT NULL,
-  `hall_id` int(10) UNSIGNED DEFAULT NULL,
-  `payment_id` int(10) UNSIGNED DEFAULT NULL,
-  `promo_id` int(10) UNSIGNED DEFAULT NULL,
-  `total` decimal(10,2) NOT NULL DEFAULT 0.00,
-  `comment` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `status` tinyint(4) NOT NULL DEFAULT 1,
-  `created_at` int(10) UNSIGNED NOT NULL,
-  `updated_at` int(10) UNSIGNED NOT NULL,
-  `created_by` int(10) UNSIGNED DEFAULT NULL,
-  `updated_by` int(10) UNSIGNED DEFAULT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `client_id` int(10) unsigned DEFAULT NULL,
+  `hall_id` int(10) unsigned DEFAULT NULL,
+  `payment_id` int(10) unsigned DEFAULT NULL,
+  `promo_id` int(10) unsigned DEFAULT NULL,
+  `total` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `comment` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT '1',
+  `created_at` int(10) unsigned NOT NULL,
+  `updated_at` int(10) unsigned NOT NULL,
+  `created_by` int(10) unsigned DEFAULT NULL,
+  `updated_by` int(10) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `client_id` (`client_id`),
   KEY `hall_id` (`hall_id`),
   KEY `payment_id` (`payment_id`),
   KEY `promo_id` (`promo_id`),
   KEY `created_by` (`created_by`),
-  KEY `updated_by` (`updated_by`)
+  KEY `updated_by` (`updated_by`),
+  CONSTRAINT `FK_record_created_by` FOREIGN KEY (`created_by`) REFERENCES `user` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `FK_record_promo_id` FOREIGN KEY (`promo_id`) REFERENCES `promo` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `FK_record_updated_by` FOREIGN KEY (`updated_by`) REFERENCES `user` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `record_ibfk_1` FOREIGN KEY (`client_id`) REFERENCES `client` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `record_ibfk_2` FOREIGN KEY (`hall_id`) REFERENCES `hall` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `record_ibfk_3` FOREIGN KEY (`payment_id`) REFERENCES `payment` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Запись';
 
---
--- Ограничения внешнего ключа сохраненных таблиц
---
+-- Экспортируемые данные не выделены.
 
---
--- Ограничения внешнего ключа таблицы `payment`
---
-ALTER TABLE `payment`
-  ADD CONSTRAINT `payment_ibfk_1` FOREIGN KEY (`client_id`) REFERENCES `client` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
-  ADD CONSTRAINT `payment_ibfk_2` FOREIGN KEY (`record_id`) REFERENCES `record` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+-- Дамп структуры для таблица pridetstudio-true.reservation
+CREATE TABLE IF NOT EXISTS `reservation` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `record_id` int(11) unsigned DEFAULT NULL,
+  `seance_start` datetime DEFAULT NULL,
+  `seance_length` int(11) DEFAULT NULL,
+  `comment` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `amount` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_reservation_record_id` (`record_id`),
+  CONSTRAINT `FK_reservation_record_id` FOREIGN KEY (`record_id`) REFERENCES `record` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Ограничения внешнего ключа таблицы `record`
---
-ALTER TABLE `record`
-  ADD CONSTRAINT `record_ibfk_1` FOREIGN KEY (`client_id`) REFERENCES `client` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
-  ADD CONSTRAINT `record_ibfk_2` FOREIGN KEY (`hall_id`) REFERENCES `hall` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
-  ADD CONSTRAINT `record_ibfk_3` FOREIGN KEY (`payment_id`) REFERENCES `payment` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
-COMMIT;
+-- Экспортируемые данные не выделены.
 
+-- Дамп структуры для таблица pridetstudio-true.user
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `auth_key` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `access_token` varchar(32) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `password_hash` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password_reset_token` varchar(32) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `role` tinyint(4) DEFAULT '1',
+  `status` tinyint(4) DEFAULT '1',
+  `created_at` int(11) unsigned NOT NULL,
+  `updated_at` int(11) unsigned NOT NULL,
+  `created_by` int(11) unsigned DEFAULT NULL,
+  `updated_by` int(11) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`),
+  KEY `FK_user_created_by` (`created_by`),
+  KEY `FK_user_updated_by` (`updated_by`),
+  CONSTRAINT `FK_user_created_by` FOREIGN KEY (`created_by`) REFERENCES `user` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `FK_user_updated_by` FOREIGN KEY (`updated_by`) REFERENCES `user` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Экспортируемые данные не выделены.
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
