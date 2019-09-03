e2201c5bfa233a1696a842fb81d77f1d52ccc1b9
Andrey Galaktionov <das.frama@gmail.com>
2019-09-03 19:17:47 +0300

    Добавил дефолтные настройки.

--- data/init/settings.php
+++ data/init/settings.php
@@ -13,4 +13,20 @@ return [
         'key' => 'calendar_interval',
         'value' => 60,
     ],
+    [
+        'key' => 'calendar_default_hall',
+        'value' => "royal",
+    ],
+    [
+        'key' => 'calendar_max_booking_range',
+        'value' => 2419200,
+    ],
+    [
+        'key' => 'calendar_max_books',
+        'value' => 0,
+    ],
+    [
+        'key' => 'calendar_default_hall',
+        'value' => "royal",
+    ],
 ];
