f4c64edecbf994ae0239d5437a3d1d83fec973bb
Andrey Galaktionov <das.frama@gmail.com>
2019-09-22 02:59:02 +0300

    Добавил вывод данных для явно указанного зала в /booking/*

--- src/http/controller/BookingController.php
+++ src/http/controller/BookingController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace app\http\controller;
 
 use app\ResponseFactory;
+use app\RequestUtils;
 use app\domain\booking\BookingDocument;
 use app\domain\setting\SettingService;
 use app\domain\hall\HallService;
@@ -55,6 +56,32 @@ class BookingController
             return $this->responder->error(ResponseFactory::NOT_FOUND, ['Hall not found.']);
         }
 
+        // Form document.
+        $bookingDoc = new BookingDocument;
+        $bookingDoc->settings = $settings;
+        $bookingDoc->hall = $hall;
+        $bookingDoc->services = $this->hallService->findServices($hall->slug, [], ['id', 'name', 'children']);
+
+        return $this->responder->success($bookingDoc);
+    }
+
+    /**
+     * Get all info at once for booking page with hall.
+     * @method GET
+     * @param ServerRequestInterface $request
+     * @return ResponseInterface
+     */
+    public function hall(ServerRequestInterface $request): ResponseInterface
+    {
+        $hallSlug = RequestUtils::getPathSegment($request, 2);
+        // Find hall.
+        $hall = $this->hallService->findBySlug($hallSlug, ['id', 'name', 'slug', 'preview_image']);
+        if ($hall === null) {
+            return $this->responder->error(ResponseFactory::NOT_FOUND, ['Hall not found.']);
+        }
+        // Find settings.
+        $settings = $this->settingService->findByGroup('calendar', ['key', 'value']);
+        $settings = array_column($settings, 'value', 'key');
         // Form document.
         $bookingDoc = new BookingDocument;
         $bookingDoc->settings = $settings;
