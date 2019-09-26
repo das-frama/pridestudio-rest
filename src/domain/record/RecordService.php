0736ff3633a1e4e8b41d7a357e4e86f5131ee22f
Andrey Galaktionov <das.frama@gmail.com>
2019-09-26 18:37:04 +0300

    Изменил логику подсчёта стоимости.

--- src/domain/record/RecordService.php
+++ src/domain/record/RecordService.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace app\domain\record;
 
+use app\entity\Coupon;
 use app\entity\Hall;
 use app\entity\PriceRule;
 use app\entity\Record;
@@ -15,9 +16,13 @@ class RecordService
     /** @var RecordRepositoryInterface */
     private $recordRepo;
 
-    public function __construct(RecordRepositoryInterface $recordRepo)
+    /** @var CouponRepositoryInterface */
+    private $couponRepo;
+
+    public function __construct(RecordRepositoryInterface $recordRepo, CouponRepositoryInterface $couponRepo)
     {
         $this->recordRepo = $recordRepo;
+        $this->couponRepo = $couponRepo;
     }
 
     /**
@@ -47,7 +52,7 @@ class RecordService
      * @param Hall $hall
      * @return int
      */
-    public function calculatePrice(Record $record, Hall $hall): int
+    public function calculatePrice(Record $record, Hall $hall, Coupon $coupon = null): int
     {
         if (empty($record->reservations)) {
             return 0;
@@ -84,18 +89,6 @@ class RecordService
      */
     private function calculatePriceRule(PriceRule $rule, object $reservation): int
     {
-        if ($rule->time_from !== null && $rule->time_to !== null) {
-            $startAt = new DateTimeImmutable();
-            $startAt = $startAt->setTimestamp($reservation->start_at);
-            list($hour, $minute) = explode(':', $rule->time_from, 2);
-            $topAt = $startAt->setTime((int) $hour, (int) $minute);
-            list($hour, $minute) = explode(':', $rule->time_to, 2);
-            $bottomAt = $startAt->setTime((int) $hour, (int) $minute);
-            $passTime = $startAt >= $topAt && $startAt < $bottomAt;
-        } else {
-            $passTime = true;
-        }
-
         switch ($rule->comparison) {
             case '=':
                 $passLength = $reservation->length == $rule->from_length;
@@ -118,12 +111,26 @@ class RecordService
             default:
                 $passLength = $reservation->length >= $rule->from_length;
         }
-        if ($passTime && $passLength) {
-            if ($rule->type == PriceRule::TYPE_PER_HOUR) {
-                return $rule->price * intval($reservation->length / 60);
-            } elseif ($rule->type == PriceRule::TYPE_FIXED) {
-                return $rule->price;
+        // Fixed price.
+        if ($rule->type == PriceRule::TYPE_FIXED && $passLength) {
+            return $rule->price;
+        }
+        // Price per hour.
+        $hoursToCount = $reservation->length / 60;
+        if ($rule->time_from !== null && $rule->time_to !== null) {
+            $date = (new DateTimeImmutable())->setTimestamp($reservation->start_at);
+            list($hour, $minute) = explode(':', $rule->time_from, 2);
+            $topAt = $date->setTime((int) $hour, (int) $minute)->getTimestamp();
+            list($hour, $minute) = explode(':', $rule->time_to, 2);
+            $bottomAt = $date->setTime((int) $hour, (int) $minute)->getTimestamp();
+            if ($hour === "00") {
+                $bottomAt += 24 * 60 * 60;
             }
+            $diff = min($reservation->start_at + $reservation->length * 60, $bottomAt) - max($reservation->start_at, $topAt);
+            $hoursToCount = $diff > 0 ? $diff / 60 / 60 : 0;
+        }
+        if ($hoursToCount > 0 && $passLength) {
+            return $rule->price * $hoursToCount;
         }
 
         return 0;
