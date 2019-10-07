285b5ab5d42720b0a3ae3edc9381c8679c8a72b7
Andrey Galaktionov <das.frama@gmail.com>
2019-10-07 17:46:23 +0300

    Подчистил код.

--- src/http/middleware/CorsMiddleware.php
+++ src/http/middleware/CorsMiddleware.php
@@ -21,8 +21,14 @@ class CorsMiddleware implements MiddlewareInterface
 
         if ($method == 'OPTIONS') {
             $response = ResponseFactory::fromStatus(200);
-            $response = $response->withHeader('Access-Control-Allow-Headers', 'Content-Type, X-XSRF-TOKEN, Authorization');
-            $response = $response->withHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, PUT, POST, DELETE, PATCH');
+            $response = $response->withHeader(
+                'Access-Control-Allow-Headers',
+                'Content-Type, X-XSRF-TOKEN, Authorization'
+            );
+            $response = $response->withHeader(
+                'Access-Control-Allow-Methods',
+                'OPTIONS, GET, PUT, POST, DELETE, PATCH'
+            );
             $response = $response->withHeader('Access-Control-Max-Age', '1728000');
             $response = $response->withHeader('Access-Control-Expose-Headers', '');
         } else {
