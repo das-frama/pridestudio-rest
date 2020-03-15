63b96382ff4d9e66562e6a2490d963bf0781477a
Andrey Galaktionov <das.frama@gmail.com>
2020-03-15 23:12:21 +0300

    Создание символьной ссылки для хранилища.

--- src/Console/Commands/AppCommand.php
+++ src/Console/Commands/AppCommand.php
@@ -27,27 +27,48 @@ class AppCommand
      */
     public function init(): int
     {
-        // Settings.
-        $data = $this->getData('settings.php');
-        if ($this->service->initSettings($data)) {
-            fwrite(STDOUT, "Settings init successfully.\n");
+        // Create public storage symlink.
+        if ($this->createStorageSymlink()) {
+            fwrite(STDOUT, "Public storage symlink created.\n");
         } else {
-            fwrite(STDOUT, "Settings already initiated.\n");
-        }
-        // Halls.
-        if ($this->service->initHalls()) {
-            fwrite(STDOUT, "Halls init successfully.\n");
-        } else {
-            fwrite(STDOUT, "Halls already initiated.\n");
-        }
-        // Coupons.
-        if ($this->service->initCoupons()) {
-            fwrite(STDOUT, "Coupons init successfully.\n");
-        } else {
-            fwrite(STDOUT, "Coupons already initiated.\n");
+            fwrite(STDERR, "Could not create a symlink.\n");
         }
 
+        // Settings.
+//        $data = $this->getData('settings.php');
+//        if ($this->service->initSettings($data)) {
+//            fwrite(STDOUT, "Settings init successfully.\n");
+//        } else {
+//            fwrite(STDOUT, "Settings already initiated.\n");
+//        }
+//        // Halls.
+//        if ($this->service->initHalls()) {
+//            fwrite(STDOUT, "Halls init successfully.\n");
+//        } else {
+//            fwrite(STDOUT, "Halls already initiated.\n");
+//        }
+//        // Coupons.
+//        if ($this->service->initCoupons()) {
+//            fwrite(STDOUT, "Coupons init successfully.\n");
+//        } else {
+//            fwrite(STDOUT, "Coupons already initiated.\n");
+//        }
+
         return 0;
+    }
+
+    /**
+     * Create symlink for public storage.
+     * @return bool
+     */
+    protected function createStorageSymlink(): bool
+    {
+        $target = join(DIRECTORY_SEPARATOR, [APP_DIR, 'storage', 'public']);
+        $link = join(DIRECTORY_SEPARATOR, [APP_DIR, 'public', 'storage']);
+        if (is_link($link)) {
+            return true;
+        }
+        return symlink($target, $link);
     }
 
     private function getData(string $filename): array
